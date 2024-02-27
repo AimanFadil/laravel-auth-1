0c87a36ee9297f60969c76cfba0f60034b1124ac
AimanFadil <aimanbello2001@gmail.com>
2024-02-27 15:59:27 +0100

    migration and seeder

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+use Faker\Generator as Faker;
+use App\Models\Type;
 
 class TypeSeeder extends Seeder
 {
@@ -12,8 +16,13 @@ class TypeSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $new_type = new Type();
+            $new_type->nome = $faker->sentence(3);
+            $new_type->slug = Str::slug($new_type->nome, '-');
+            $new_type->save();
+        }
     }
 }
